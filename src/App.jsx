5295df32aaf14f15562b0de7ca0426f8202a1f1a
Adla Gomes <adlagomes@gmail.com>
2023-01-05 14:21:13 -0300

    add getCardClicked
    adicionada função que pega o nome do pokemon da carta que foi clicada e compara com o nome do pokemon que aparece na mensagem

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,20 @@ import "./App.css";
 
 function App() {
   const [pokeList, setPokeList] = useState([]);
-  const [board, setBoard] = useState(false);
+  const [namePokemon, setNamePokemon] = useState();
 
-  const [cardClicked, setCardClicked] = useState()
+  const [board, setBoard] = useState(false);
+  // const [cardClicked, setCardClicked] = useState();
   const [message, setMessage] = useState(
     <h2>Clique em <span>Start</span> para começar</h2>
   );
 
   const [styleCard, setStyleCard] = useState("frontCard");
   
-
   const pokeApi = async () => {
     const response = await fetch("https://pokeapi.co/api/v2/pokemon/");
     const data = await response.json();
-    const pokeData = data.results; // array de objetos
+    const pokeData = data.results;
 
     pokeData.map(({ url, name }) => getPokeData(url, name));
   };
@@ -36,6 +36,7 @@ function App() {
         },
       ].slice(0, 9)
     );
+
     setMessage(<h2>contando...</h2>)
   };
 
@@ -47,26 +48,20 @@ function App() {
   function boardOnOff() {
     if (pokeList[8]) {
       setStyleCard("backCard")
+      setNamePokemon(pokeList[ramdonId()].name)
       setBoard(true)
     }
   }
 
-    function ramdonId() {
-    const idNumber = Math.floor(Math.random() * (10 - 1) + 0);
-    return idNumber;
-  }
-
   const getCardClicked = r => {
-    const cardTaked = r
-    console.log("testeDoResultado: " + cardTaked)
-    if (cardTaked === "bulbasaur") {
+    const chosenCard = r
+    console.log("pokemon: " + chosenCard)
+    if (chosenCard === namePokemon) {
       console.log("you win")
     } else {
       console.log("loooose")
     }
   }
-
-  // se a carta na qual cliquei tiver o mesmo nome que aparece na question.. então eu acertei. se nao... eu errei.
 
   useEffect(() => {
     const timer = setTimeout(boardOnOff, 5000);
@@ -80,7 +75,7 @@ function App() {
       <h1 className="title">~Pokememo~</h1>
       <div className="question_bar">
         {
-          board ? <h2>Onde está <span>{pokeList[ramdonId()].name}</span>?</h2> : message
+          board ? <h2>Onde está <span>{namePokemon}</span>?</h2> : message
         }
       </div>
       <div className="table">
