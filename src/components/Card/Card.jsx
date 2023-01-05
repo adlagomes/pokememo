81fe05623ead588fe8839a93613c21f9bc087e2a
Adla Gomes <adlagomes@gmail.com>
2023-01-05 14:22:20 -0300

    add getCard
    função pega o nome da carta que recebeu clique e passa para a função se App.jsx. Função também adiciona id na carta clicada para mudar a aparencia dela.

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,16 +2,21 @@ import { useState } from "react";
 import "./styles.css"
 
 export function Card(props) {
+  const [style, setStyle] = useState("")
 
   const getCard = (e) => {
     e.preventDefault();
     let cardName = props.name //tentando pegar a carta clicada
-    // console.log("nome da carta: " + cardName)
     props.takingTheCard(cardName)
+    if (props.class !== "frontCard") {
+      setStyle("virar")
+    } else {
+      return
+    }
   }
 
   return (
-    <div className={props.class} onClick={getCard}>
+    <div className={props.class} onClick={getCard} id={style}>
       <img src={props.img} alt="" />
       <span>{props.name}</span>
     </div>
