e62ecf11e4d28e74f0fb084037208e9aba484fcb
Adla Gomes <adlagomes@gmail.com>
2023-01-06 09:33:33 -0300

    add new component
    adicionei um novo componente para o enunciado

---
+++ src/components/Statement/index.jsx
@@ -0,0 +1,9 @@
+import "./style.css"
+
+export const Statement = () => {
+  return (
+    <div className="message">
+      <h2>Clique em <span>Start</span> para começar.<br/>Você tem 5 segundos para memorizar as cartas</h2>
+    </div>
+  );
+}
